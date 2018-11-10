e8c768796e6e372ef1d0520ff8206bb0df476605
SphinxCrypto <43366301+SphinxCrypto@users.noreply.github.com>
2018-11-10 01:10:40 -0400

    Update add_install.sh

--- add_install.sh
+++ add_install.sh
@@ -200,7 +200,7 @@ fi
 }
 
 function prepare_system() {
-echo -e "Preparing the system to install ${GREEN}$COIN_NAME${NC} master node."
+echo -e "Preparing the system to install ${GREEN}$COIN_NAME${NC} masternode."
 apt-get update >/dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
